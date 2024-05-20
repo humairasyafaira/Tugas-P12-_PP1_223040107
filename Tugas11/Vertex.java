465ef4001e05d2e6f79aee25a9162d7792bab2fa
humairasyafaira <117334650+humairasyafaira@users.noreply.github.com>
2024-05-20 20:11:10 +0700

    Add files via upload

---
+++ Tugas11/Vertex.java
@@ -0,0 +1,33 @@
+package Tugas11;
+
+public class Vertex {
+    private char label; // Label dari vertex (misalnya 'A')
+    private boolean wasVisited; 
+
+    // Konstruktor untuk inisialisasi label dan set wasVisited menjadi false
+    public Vertex(char lab) {
+        this.label = lab;
+        this.wasVisited = false;
+    }
+
+    // Getter untuk label
+    public char getLabel() {
+        return label;
+    }
+
+    // Setter untuk label
+    public void setLabel(char label) {
+        this.label = label;
+    }
+
+    // Getter untuk wasVisited
+    public boolean wasVisited() {
+        return wasVisited;
+    }
+
+    // Setter untuk wasVisited
+    public void setVisited(boolean wasVisited) {
+        this.wasVisited = wasVisited;
+    }
+}
+
